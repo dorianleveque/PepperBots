d0af23e371e237440a930c300f254c01d87916c9
dorianleveque <dorian.leveque@outlook.fr>
2020-11-30 15:52:43 +0100

    refactoring

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 from src.Simulation import Simulation
 #from src.Simulation import Simulation
 from src.TrainPerception import TrainPerception
@@ -6,6 +7,7 @@ from src.TrainPerception import TrainPerception
 
 def runSimulation():
     sim = Simulation()
+    sim.start()
 
 
 def trainPerception():
@@ -17,18 +19,16 @@ def trainCommunication():
 
 
 if __name__ == "__main__":
+ 
+    parser = argparse.ArgumentParser(description="IML projet to test some ai feature and human / robot interaction.\nWe using qiBullet and machine learning.")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-t", "--train", action="append", choices=('communication', 'perception'), help="train model")
+    args = parser.parse_args()
 
-    if (len(sys.argv) > 2):
-        if (sys.argv[1] in ["-t", "--train"] and len(sys.argv) > 2):
-            if (sys.argv[2] == "perception"):
-                trainPerception()
-            elif(sys.argv[2] == "communication"):
-                trainCommunication()
-            
-        else:
-            print("""Usage:
-                -t --train <module>     to train module specified
-            """)
-
+    if args.train:
+        if ("perception" in args.train): 
+            trainPerception()
+        elif("communication" in args.train):
+            trainCommunication()
     else:
         runSimulation()
