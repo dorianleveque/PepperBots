152f2e229754ef7ad9220a872ca0d079006f8abf
Elouan-E <elouaneven@gmail.com>
2020-11-30 17:43:49 +0100

    Add laser

--- src/Robot.py
+++ src/Robot.py
@@ -7,6 +7,9 @@ from src.Perception import Perception
 
 
 class Robot(Thread):
+
+    min_laser_value = 1.5 # Constante 
+
     def __init__(self, sim, client):
         Thread.__init__(self)
         #self.th = Thread(target=pepperBehavior)
@@ -19,18 +22,33 @@ class Robot(Thread):
     
 
     def run(self):
+        pepper.showLaser(True)
+        pepper.subscribeLaser()
+
         while True:
-            print("hello")
+            self.idle()
+            detectLeft, detectFront, detectRight = self.checkLasers()
+            time.sleep(3)
         #self.threads[1].save_img()
     
     def moveTo(self,x,y): 
         self.pepper.moveTo(x,y,math.atan2(y,x))
 
-    def idle(self): # Thread fonction
+    def idle(self): # Pepper flex ! 
         self.pepper.goToPosture("Stand", 1)
         time.sleep(2.0)
         self.pepper.goToPosture("Crouch", 1)
 
-    def pepperBehavior(self):
-        self.idle()
+    def checkLasers(self):
+        laserRight = pepper.getRightLaserValue()
+        laserFront = pepper.getFrontLaserValue()
+        laserLeft = pepper.getLeftLaserValue()
+        detectRight = lambda x : True if(laser <= min_laser_value for laser in laserRight) else False
+        detectFront = lambda x : True if(laser <= min_laser_value for laser in laserFront) else False
+        detectLeft = lambda x : True if(laser <= min_laser_value for laser in laserLeft) else False
+        return detectLeft, detectFront, detectRight
+
+        
+
+
     
